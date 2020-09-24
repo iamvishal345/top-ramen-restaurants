752cfc245e7fe5f753406d0fe4335a52059d34ff
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-24 18:25:59 +0530

    Updates

---
+++ precache-manifest.69f476d204b9c221c929e41ff4ff1463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6aef87b47758f99ea9d86a7a16fe859",
+    "url": "/ramen-restaurants/index.html"
+  },
+  {
+    "revision": "e0bb86da29b7b08a7db8",
+    "url": "/ramen-restaurants/static/css/main.7dbbd7bd.chunk.css"
+  },
+  {
+    "revision": "a3c169c04518e1b8082b",
+    "url": "/ramen-restaurants/static/js/2.4694aad4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ramen-restaurants/static/js/2.4694aad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bb86da29b7b08a7db8",
+    "url": "/ramen-restaurants/static/js/main.1f8dbee9.chunk.js"
+  },
+  {
+    "revision": "3f6f0c244b74aa55e191",
+    "url": "/ramen-restaurants/static/js/runtime-main.613f7b00.js"
+  }
+]);
